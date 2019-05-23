7389be5abaa7186f4e75f3bf237d4504fb93a5f7
Gauraviitkgp <gauravs123456789@gmail.com>
2019-05-22 21:26:45 -0400

    First Very successful code

--- src/brightness_contrast.cpp
+++ src/brightness_contrast.cpp
@@ -6,6 +6,8 @@ using std::cin;
 using std::cout;
 using std::endl;
 using namespace cv;
+using namespace std;
+
 int main( int argc, char** argv )
 {
     CommandLineParser parser( argc, argv, "{@input | ../data/lena.jpg | input image}" );
@@ -17,22 +19,32 @@ int main( int argc, char** argv )
       return -1;
     }
     Mat new_image = Mat::zeros( image.size(), image.type() );
-    double alpha = 1.0; /*< Simple contrast control */
+    int alpha = 1.0; /*< Simple contrast control */
     int beta = 0;       /*< Simple brightness control */
     cout << " Basic Linear Transforms " << endl;
     cout << "-------------------------" << endl;
-    cout << "* Enter the alpha value [1.0-3.0]: "; cin >> alpha;
-    cout << "* Enter the beta value [0-100]: ";    cin >> beta;
-    for( int y = 0; y < image.rows; y++ ) {
-        for( int x = 0; x < image.cols; x++ ) {
-            for( int c = 0; c < image.channels(); c++ ) {
-                new_image.at<Vec3b>(y,x)[c] =
-                  saturate_cast<uchar>( alpha*image.at<Vec3b>(y,x)[c] + beta );
+
+    namedWindow("Original Image", WINDOW_NORMAL);
+    namedWindow("New Image", WINDOW_NORMAL);
+    namedWindow("Sliders");
+
+    createTrackbar("H_r","Sliders",&alpha, 100); //Creating sliders
+    createTrackbar("S_r","Sliders",&beta, 100);
+
+    while(1)
+    {
+        for( int y = 0; y < image.rows; y++ ) {
+            for( int x = 0; x < image.cols; x++ ) {
+                for( int c = 0; c < image.channels(); c++ ) {
+                    new_image.at<Vec3b>(y,x)[c] =
+                      saturate_cast<uchar>( alpha*image.at<Vec3b>(y,x)[c] + beta );
+                }
             }
         }
+
+        imshow("Original Image", image);
+        imshow("New Image", new_image);
+        waitKey(30);
     }
-    imshow("Original Image", image);
-    imshow("New Image", new_image);
-    waitKey();
     return 0;
 }
