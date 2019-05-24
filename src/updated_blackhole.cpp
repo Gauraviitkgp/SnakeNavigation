36bfa80713183b88183f1f308e81b0e1851afb94
Gauraviitkgp <gauravs123456789@gmail.com>
2019-05-23 22:19:40 -0400

    Added some new files, improved code

--- src/updated_blackhole.cpp
+++ src/updated_blackhole.cpp
@@ -10,7 +10,14 @@ using namespace std;
 
 Mat hsv_img; //Global Declaration
 Mat img, img_corrected, img_gamma_corrected;
+Mat red_thr,white_thr,black_thr,p0,high_pr,required; //Declaration of Image names we need. Mat stands for Matrix of image.
 
+int h_r=0,s_r=210,ht_r=23,st_r=47; //Hue Red, Saturation red, Hue Threshold, Saturation threshold
+int r_w=124, g_w=133,b_w=136, rt_w=120, gt_w=86,bt_w=73 ;//Red white, Green white, Blue White
+int r_b=0,g_b=0,b_b=0,rt_b=110,gt_b=110,bt_b=20; //Red black, 
+
+int alpha = 360;
+int beta = 00;
 
 void mouseCB(int event, int x, int y, int flags, void* userdata) // Mouse Callback on Hsv image. Outputs the pixel value on the pixel mouse was pressed
 {
@@ -27,28 +34,63 @@ void basicLinearTransform(const double alpha_, const int beta_)
 }
 
 
-int main(int argc, char** argv)
+void contrast_brighten()
 {
-	string name=argv[1];//Taking image or Video name from command line
-	char a;
-	cout<<"image(i) or video(v)"; 
-	cin>>a;
-
-	Mat red_thr,white_thr,black_thr,p0,high_pr,required; //Declaration of Image names we need. Mat stands for Matrix of image.
-
-	int h_r=0,s_r=210,ht_r=23,st_r=47; //Hue Red, Saturation red, Hue Threshold, Saturation threshold
-	int r_w=124, g_w=133,b_w=136, rt_w=120, gt_w=86,bt_w=73 ;//Red white, Green white, Blue White
-	int r_b=0,g_b=0,b_b=0,rt_b=110,gt_b=110,bt_b=20; //Red black, 
-
-	int alpha = 360;
-	int beta = 00;
-
-	//values accepted are (h_r-ht_r, h_r+ht_r)
-	namedWindow("Sliders"); //Slider Interface
-	namedWindow("MainIM", WINDOW_NORMAL);
 	namedWindow("Brightness and contrast adjustments", WINDOW_NORMAL);
-	setMouseCallback("MainIM", mouseCB, NULL);
+	createTrackbar("Alpha gain (contrast)", "Brightness and contrast adjustments", &alpha, 500);
+    createTrackbar("Beta bias (brightness)", "Brightness and contrast adjustments", &beta, 200);
 
+    double alpha_value = alpha / 100.0;
+    int beta_value = beta - 100;
+    basicLinearTransform(alpha_value, beta_value);
+    cvtColor(img_corrected, hsv_img, COLOR_BGR2HSV_FULL); 
+    GaussianBlur( img_corrected, img_corrected, Size(3, 3), 2, 2 );//Applying Gaussian Blur of parameter 1,1
+    GaussianBlur( img_corrected, img_corrected, Size(3, 3), 2, 2 );//Applying Gaussian Blur of parameter 1,1
+}
+
+void erosion(int total_times,int size, Mat On, Mat otpt)
+{
+	int erosion_type;//Erosion and Dilation initial declaration
+	erosion_type = MORPH_RECT;//Erosion type
+	int erosion_size=size; //size of the kernel will be erosion_size*2+1
+	Mat element =  getStructuringElement( erosion_type,
+		Size( 2*erosion_size + 1, 2*erosion_size+1 ),
+		Point( erosion_size, erosion_size ) );
+
+	for (int i = 0; i < total_times; i++)
+		erode(On,otpt, element );
+}
+
+void dilation(int total_times,int size, Mat On, Mat otpt)
+{
+	int erosion_type;//Erosion and Dilation initial declaration
+	erosion_type = MORPH_RECT;//Erosion type
+	int erosion_size=size; //size of the kernel will be erosion_size*2+1
+	Mat element =  getStructuringElement( erosion_type,
+		Size( 2*erosion_size + 1, 2*erosion_size+1 ),
+		Point( erosion_size, erosion_size ) );
+
+	for (int i = 0; i < total_times; i++)
+		dilate(On,otpt, element );
+}
+
+void threshold()
+{
+	inRange(hsv_img,Scalar(h_r-ht_r,s_r-st_r,0), Scalar(h_r+ht_r,s_r+st_r,255),red_thr);//Thresholding, 1 if value lie between [(h_r-ht_r,s_r-st_r,0),(h_r+ht_r,s_r+st_r,255)] else 0.                  ,
+	inRange(img_corrected,Scalar(r_w-rt_w,g_w-gt_w,b_w-bt_w), Scalar(r_w+rt_w,g_w+gt_w,b_w+bt_w),white_thr);
+	inRange(img_corrected,Scalar(r_b-rt_b,g_b-gt_b,b_b-bt_b), Scalar(r_b+rt_b,g_b+gt_b,b_b+bt_b),black_thr);
+}
+
+void bit_opr()
+{
+	bitwise_or(red_thr,white_thr,p0);//Img_A[x][y]+Img_B[x][y]
+	bitwise_not(p0, high_pr);//Img_A[x][y]'
+	bitwise_and(high_pr,black_thr,required);//Img_A[x][y].Img_B[x][y]
+}
+
+void create_trackbars()
+{
+	namedWindow("Sliders"); //Slider Interface
 	createTrackbar("H_r","Sliders",&h_r,255); //Creating sliders
 	createTrackbar("S_r","Sliders",&s_r,255);
 	createTrackbar("HT_r","Sliders",&ht_r,255);
@@ -67,9 +109,22 @@ int main(int argc, char** argv)
 	createTrackbar("rt_b","Sliders",&rt_b,255);
 	createTrackbar("gt_b","Sliders",&gt_b,255);
 	createTrackbar("bt_b","Sliders",&bt_b,255);
+}
 
-	createTrackbar("Alpha gain (contrast)", "Brightness and contrast adjustments", &alpha, 500);
-    createTrackbar("Beta bias (brightness)", "Brightness and contrast adjustments", &beta, 200);
+int main(int argc, char** argv)
+{
+	string name=argv[1];//Taking image or Video name from command line
+	char a;
+	cout<<"image(i) or video(v)"; 
+	cin>>a;
+
+	//values accepted are (h_r-ht_r, h_r+ht_r)
+
+	create_trackbars();
+
+	namedWindow("MainIM", WINDOW_NORMAL);
+	
+	setMouseCallback("MainIM", mouseCB, NULL);
 
 
 	if (a=='i') //If image
@@ -84,40 +139,15 @@ int main(int argc, char** argv)
 		imshow("MainIM",img);
 		while(1)
 		{
-
-			double alpha_value = alpha / 100.0;
-		    int beta_value = beta - 100;
-		    basicLinearTransform(alpha_value, beta_value);
-		    cvtColor(img_corrected, hsv_img, COLOR_BGR2HSV_FULL); 
-		    GaussianBlur( img_corrected, img_corrected, Size(3, 3), 2, 2 );//Applying Gaussian Blur of parameter 1,1
-		    GaussianBlur( img_corrected, img_corrected, Size(3, 3), 2, 2 );//Applying Gaussian Blur of parameter 1,1
+			contrast_brighten();
 			//No thresholding has been done on HSV images with V channel as it stands for brightness
-			inRange(hsv_img,Scalar(h_r-ht_r,s_r-st_r,0), Scalar(h_r+ht_r,s_r+st_r,255),red_thr);//Thresholding, 1 if value lie between [(h_r-ht_r,s_r-st_r,0),(h_r+ht_r,s_r+st_r,255)] else 0.                  ,
-			inRange(img_corrected,Scalar(r_w-rt_w,g_w-gt_w,b_w-bt_w), Scalar(r_w+rt_w,g_w+gt_w,b_w+bt_w),white_thr);
-			inRange(img_corrected,Scalar(r_b-rt_b,g_b-gt_b,b_b-bt_b), Scalar(r_b+rt_b,g_b+gt_b,b_b+bt_b),black_thr);
+			threshold();
 
-			bitwise_or(red_thr,white_thr,p0);//Img_A[x][y]+Img_B[x][y]
-			bitwise_not(p0, high_pr);//Img_A[x][y]'
-			bitwise_and(high_pr,black_thr,required);//Img_A[x][y].Img_B[x][y]
-
-
-			int erosion_type;//Erosion and Dilation initial declaration
-			erosion_type = MORPH_RECT;//Erosion type
-			int erosion_size=1; //size of the kernel will be erosion_size*2+1
-			Mat element =  getStructuringElement( erosion_type,
-			Size( 2*erosion_size + 1, 2*erosion_size+1 ),
-			Point( erosion_size, erosion_size ) );
-
-			erode( required, required , element );
-			// dilate( required, required , element );//Dilate on Required to output Required with element 'element'
-			dilate( required, required , element );
-			dilate( required, required , element );
-			dilate( required, required , element );
-			dilate( required, required , element );
-			// dilate( required, required , element );
+			bit_opr();
 			
+			erosion(1,1,required,required);
+			dilation(1,4,required,required);
 			
-
 			namedWindow("hsv_img", WINDOW_NORMAL ); //Window Naming
 			namedWindow("red_thr", WINDOW_NORMAL );
 			namedWindow("black_thr", WINDOW_NORMAL );
@@ -153,40 +183,19 @@ int main(int argc, char** argv)
 		{
 			static int i=0; //Counter
 			cap >> img; //Store Videocapture output on img
-			Mat required, p0,high_pr,black_th;
 			// cvtColor(img, hsv_img, COLOR_BGR2HSV_FULL);
 
-			img_corrected = Mat(img.rows, img.cols, img.type());
-			double alpha_value = alpha / 100.0;
-		    int beta_value = beta - 100;
-		    basicLinearTransform(alpha_value, beta_value);
-		    cvtColor(img_corrected, hsv_img, COLOR_BGR2HSV_FULL); 
-		    GaussianBlur( img_corrected, img_corrected, Size(3, 3), 2, 2 );//Applying Gaussian Blur of parameter 1,1
-		    GaussianBlur( img_corrected, img_corrected, Size(3, 3), 2, 2 );//Applying Gaussian Blur of parameter 1,1
+			contrast_brighten();
+			//No thresholding has been done on HSV images with V channel as it stands for brightness
+			threshold();
 
-
-			inRange(hsv_img,Scalar(h_r-ht_r,s_r-st_r,0), Scalar(h_r+ht_r,s_r+st_r,255),red_thr);//Thresholding, 1 if value lie between [(h_r-ht_r,s_r-st_r,0),(h_r+ht_r,s_r+st_r,255)] else 0.                  ,
-			inRange(img_corrected,Scalar(r_w-rt_w,g_w-gt_w,b_w-bt_w), Scalar(r_w+rt_w,g_w+gt_w,b_w+bt_w),white_thr);
-			inRange(img_corrected,Scalar(r_b-rt_b,g_b-gt_b,b_b-bt_b), Scalar(r_b+rt_b,g_b+gt_b,b_b+bt_b),black_thr);
-
-			bitwise_or(red_thr,white_thr,p0);
-			bitwise_not(p0, high_pr);
-			bitwise_and(high_pr,black_thr,required);
-
-
-			int erosion_type;
-			erosion_type = MORPH_RECT;
-			int erosion_size=1; //size of the kernel will be erosion_size*2+1
-			Mat element =  getStructuringElement( erosion_type,
-			Size( 2*erosion_size + 1, 2*erosion_size+1 ),
-			Point( erosion_size, erosion_size ) );
-
-			erode( required, required , element );
-			// dilate( required, required , element );//Dilate on Required to output Required with element 'element'
-			dilate( required, required , element );
-			dilate( required, required , element );
-			dilate( required, required , element );
-			dilate( required, required , element );
+			bit_opr();
+			
+			erosion(1,2,required,required);
+			GaussianBlur( required, required, Size(3, 3), 2, 2 );//Applying Gaussian Blur of parameter 1,1
+			GaussianBlur( required, required, Size(3, 3), 2, 2 );//Applying Gaussian Blur of parameter 1,1
+			GaussianBlur( required, required, Size(3, 3), 2, 2 );//Applying Gaussian Blur of parameter 1,1
+			// dilation(1,2,required,required);
 
 			namedWindow("hsv_img", WINDOW_NORMAL );
 			namedWindow("red_thr", WINDOW_NORMAL );
@@ -203,7 +212,7 @@ int main(int argc, char** argv)
 			imshow("p0"			,p0  		);
 			imshow("high_pr"	,high_pr  	);			
 			imshow("required"	,required  	);
-			imshow("MAINIM"		,img);
+			imshow("MainIM"		,img);
 			// imwrite("../vid_output/hsv_img"+name.substr(9,10)+".jpg",hsv_img	);
 			// imwrite("../vid_output/red_thr"+name.substr(9,10)+".jpg"	,red_thr 	);
 			// imwrite("../vid_output/black_thr"+name.substr(9,10)+".jpg"	,black_thr  );
@@ -212,7 +221,7 @@ int main(int argc, char** argv)
 			// imwrite("../vid_output/high_pr"+name.substr(9,10)+".jpg"	,high_pr  	);			
 			imwrite("../vid_output/required"+ to_string(i)+".jpg"	,required  	);
 			i++;
-			waitKey(33);
+			waitKey(100);
 		}
 		cap.release();//Releasing all allocated memories to object cap
 	    destroyAllWindows();// Destroying all windows
